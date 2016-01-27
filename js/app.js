7992f2febd20c234893b11393d22f3e27de932ea
Merve Silk <merve.silk@gmail.com>
2016-01-27 16:22:21 +0000

    Uses hide and show directives to change product visibility

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@
     name : "Sapphire Crystal",
     price : "13.65",
     colour : "Blue",
+    canPurchase : true,
+    soldOut : true,
     description : "These crystals have a good action within your brain to assist learning. They help you to feel greater mental discipline and might also kindle lucid dreaming.",
   }
 
