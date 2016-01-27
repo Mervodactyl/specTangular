20c598e95d5d2303d503f09da4c0ce89896d2600
Merve Silk <merve.silk@gmail.com>
2016-01-27 15:31:00 +0000

    Creates controller and accesses it through angular directives in the html

---
+++ js/app.js
@@ -0,0 +1,16 @@
+(function() {
+  var app = angular.module("store", []);
+
+  app.controller("StoreController", function(){
+    this.product = jewel;
+  });
+
+  var jewel = {
+    name : "Sapphire Crystal",
+    price : "13.65",
+    colour : "Blue",
+    description : "These crystals have a good action within your brain to assist learning. They help you to feel greater mental discipline and might also kindle lucid dreaming.",
+  }
+
+
+})();
