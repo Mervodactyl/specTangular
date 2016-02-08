aecf5f88f995dedbc02fce1bd051873ba2ab862a
Merve Silk <merve.silk@gmail.com>
2016-02-08 11:47:09 +0000

    Creates new Controller Panel and removes logic from index, plus new function expressions to check tab vallues

--- js/app.js
+++ js/app.js
@@ -81,5 +81,16 @@
   }
 ]
 
+  app.controller("PanelController", function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
 
 })();
